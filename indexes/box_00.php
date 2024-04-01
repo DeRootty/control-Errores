bb5be690fda3f5f749239f6fa4e4ccf767b8fda4
David <152688771+DeRootty@users.noreply.github.com>
2024-04-01 09:06:29 +0200

    Add files via upload

---
+++ indexes/box_00.php
@@ -0,0 +1,125 @@
+<?php declare(strict_types=1);
+    namespace box_00;
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace practicasAPP\box_00;
+    
+    try{
+        if(!file_exists("/srv/vhost/derootty.xyz/home/html/Dinamica/seguridad/ahead.php")){
+            throw new Exception("Fallo en el fundamento inherente a la seguridad. El archivo ".__FILE__." Viola el acceso al recurso");
+        }
+    } catch (Exception $ex) {
+        echo $ex->getMessage();
+        exit;
+    }finally{
+        require_once ("/srv/vhost/derootty.xyz/home/html/Dinamica/seguridad/ahead.php");
+        //include ("/srv/vhost/derootty.xyz/home/html/Dinamica/seguridad/ahead.php");
+    }
+
+  class indexesARCH{
+
+    public array $indexARCH;
+    private array $idIndexARCH;
+    private array $LyIndexARCH;
+    private string $enProceso;
+
+    public function __construct(string $rootFile){
+
+      $this->enProceso = $rootFile;
+      $phh=0;
+      $ordtohex=array(
+        "str"=>array(),
+        "ord"=>array(),
+        "hex"=>array()
+      );
+      $prorratt=array();
+      $ordtohex["str"] = explode("", $rootFile);
+      foreach($ordtohex["str"] as $idVal => $valEnd){
+        $phh++;
+        array_push($ordtohex["ord"], ord($valEnd));
+        array_push($ordtohex["hex"], dechex(ord($valEnd)));
+      }
+      array_push($prorratt, $phh/8);
+      array_push($prorratt, $phh%8);
+
+      if (getmyinode() == fileinode($rootFile)) {
+        $this->idIndexARCH = array(
+          dechex($ii++)."x" => 0,
+          dechex($ii++)."x" => 1,
+          dechex($ii++)."x" => 2,
+          dechex($ii++)."x" => 3,
+          dechex($ii++)."x" => 4,
+          dechex($ii++)."x" => 5,
+          dechex($ii++)."x" => 6,
+          dechex($ii++)."x" => 7,
+          dechex($ii++)."x" => 8
+        );
+        $opciones = [
+          'cost' => 12,
+        ];
+
+        $this->LyIndexARCH = array_flip($this->idIndexARCH);
+        $ii=-1;
+        $this->indexARCH = array("binario"=>array());
+        $this->indexARCH = array("hash"=>array());
+        $this->indexARCH["binario"][$this->LyIndexARCH[$ii++]] = array();
+        $this->indexARCH["binario"][$this->LyIndexARCH[$ii++]] = array();
+        $this->indexARCH["binario"][$this->LyIndexARCH[$ii++]] = array();
+        $this->indexARCH["binario"][$this->LyIndexARCH[$ii++]] = array();
+        $this->indexARCH["binario"][$this->LyIndexARCH[$ii++]] = array();
+        $this->indexARCH["binario"][$this->LyIndexARCH[$ii++]] = array();
+        $this->indexARCH["binario"][$this->LyIndexARCH[$ii++]] = array();
+        $this->indexARCH["binario"][$this->LyIndexARCH[$ii++]] = array();
+        $this->indexARCH["hash"][$this->LyIndexARCH[$ii++]] = array();
+        $this->indexARCH["hash"][$this->LyIndexARCH[$ii++]] = array();
+        $this->indexARCH["hash"][$this->LyIndexARCH[$ii++]] = array();
+        $this->indexARCH["hash"][$this->LyIndexARCH[$ii++]] = array();
+        $this->indexARCH["hash"][$this->LyIndexARCH[$ii++]] = array();
+        $this->indexARCH["hash"][$this->LyIndexARCH[$ii++]] = array();
+        $this->indexARCH["hash"][$this->LyIndexARCH[$ii++]] = array();
+        $this->indexARCH["hash"][$this->LyIndexARCH[$ii++]] = array();
+        //echo password_hash("rasmuslerdorf", PASSWORD_BCRYPT, $opciones)."\n";
+      }
+    }
+
+    /**
+     * Firma de la pagina para instancia de carga
+     *
+     */
+
+    private function instanciaPagina(string $instanciaFirma): int{
+      array_push($this->indexARCH["binario"][$this->LyIndexARCH[$ii++]], base64_encode(dechex(filectime($instanciaFirma))));
+      array_push($this->indexARCH["binario"][$this->LyIndexARCH[$ii++]], base64_encode(dechex(fileatime($instanciaFirma))));
+      array_push($this->indexARCH["binario"][$this->LyIndexARCH[$ii++]], base64_encode(getmyinode()));
+      array_push($this->indexARCH["binario"][$this->LyIndexARCH[$ii++]], base64_encode(dechex(fileinode($instanciaFirma))));
+      array_push($this->indexARCH["binario"][$this->LyIndexARCH[$ii++]], base64_encode(dechex(filegroup($instanciaFirma))));
+      array_push($this->indexARCH["binario"][$this->LyIndexARCH[$ii++]], base64_encode(dechex(fileowner($instanciaFirma))));
+      array_push($this->indexARCH["binario"][$this->LyIndexARCH[$ii++]], base64_encode("David Salado Rodriguez"));
+      array_push($this->indexARCH["binario"][$this->LyIndexARCH[$ii--]], base64_encode(implode("", $this->indexARCH["hash"])));
+
+      array_push($this->indexARCH["hash"][$this->LyIndexARCH[$ii--]], base64_encode(password_hash(dechex(filectime($instanciaFirma)), PASSWORD_BCRYPT, $opciones)));
+      array_push($this->indexARCH["hash"][$this->LyIndexARCH[$ii--]], base64_encode(password_hash(dechex(fileatime($instanciaFirma)), PASSWORD_BCRYPT, $opciones)));
+      array_push($this->indexARCH["hash"][$this->LyIndexARCH[$ii--]], base64_encode(password_hash(getmyinode())));
+      array_push($this->indexARCH["hash"][$this->LyIndexARCH[$ii--]], base64_encode(password_hash(dechex(fileinode($instanciaFirma)), PASSWORD_BCRYPT, $opciones)));
+      array_push($this->indexARCH["hash"][$this->LyIndexARCH[$ii--]], base64_encode(password_hash(dechex(filegroup($instanciaFirma)), PASSWORD_BCRYPT, $opciones)));
+      array_push($this->indexARCH["hash"][$this->LyIndexARCH[$ii--]], base64_encode(password_hash(dechex(fileowner($instanciaFirma)), PASSWORD_BCRYPT, $opciones)));
+      array_push($this->indexARCH["hash"][$this->LyIndexARCH[$ii--]], base64_encode(password_hash("David Salado Rodriguez", PASSWORD_BCRYPT, $opciones)));
+      array_push($this->indexARCH["hshOne"][$this->LyIndexARCH[$ii--]], base64_encode(password_hash(implode("", $this->indexARCH["hash"]), PASSWORD_BCRYPT, $opciones)));
+
+      return -1;
+    }
+
+    public function establecePagina(string $ruta): bool{
+      if(in_array($ruta, $this->indexARCH)){
+        return true;
+      }
+      echo "<pre>";
+      print_r($this->indexARCH);
+      echo "</pre>";
+      //array_push($this->indexARCH, $ruta);
+    }
+  }
+ 
---
+++ indexes/sub_box_01_Ix01A.php
@@ -0,0 +1,113 @@
+<?php declare(strict_types=1);
+    //loop para indice
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+    try{
+        if(!file_exists("/srv/vhost/derootty.xyz/home/html/Dinamica/seguridad/ahead.php")){
+            throw new Exception("Fallo en el fundamento inherente a la seguridad. El archivo " . __FILE__ . " Viola el acceso al recurso");
+        }
+        if(!isset($this)){
+            throw new Exception("El archivo " . __FILE__ . " No heredas los permisos de ejecucion<br>\n");
+        }else if(!isset($this->esqueletoHTML)){
+            throw new Exception("El archivo " . __FILE__ . " No heredas los permisos de ejecucion<br>\n");
+        }
+        if(!isset($this->modoTest)){
+            throw new Exception("El archivo " . __FILE__ . " No heredas los permisos de ejecucion<br>\n");
+        }
+    } catch (Exception $ex) {
+        echo $ex->getMessage();
+        exit;
+    }finally{
+        if(!file_exists(BASE_PATH . RENDER_PATH . "/inicio.php")){
+            array_push($salida["A"], $this->modoTest);
+            array_push($salida["B"], "inicioHTML"." - Error en " . __FILE__ . " Linea: " . __LINE__);
+            array_push($salida["C"], "Ruta inexistente " . BASE_PATH . RENDER_PATH . "/inicio.php");
+            array_push($salida["D"], $check);
+            array_push($salida["E"], true);
+            return $salida;
+        }
+    }
+    
+    try{
+        //echo "En asignacion Indice Modo test en true<br>\n";
+        if(count($this->index)==0 && count($this->salidaFinHTML)>0 ){
+            require_once(BASE_PATH . RENDER_PATH . "/inicio.php");
+            array_push($salida["A"], $this->modoTest);
+            array_push($salida["B"], "inicioHTML");
+            array_push($salida["C"], "entrada a inicio<br>\n");
+            array_push($salida["D"], $check);
+            array_push($salida["E"], false);
+        }else if(count($this->index)==0 && count($this->salidaFinHTML)>0 ){
+            array_push($salida["A"], $this->modoTest);
+            array_push($salida["B"], "inicioHTML");
+            array_push($salida["C"], "reentrada a inicio<br>\n");
+            array_push($salida["D"], $check);
+            array_push($salida["E"], false);
+        }else if(count($this->index)>0 && count($this->salidaFinHTML)>0 ){
+            array_push($salida["A"], $this->modoTest);
+            array_push($salida["B"], "inicioHTML");
+            array_push($salida["C"], "definicion por controlar en error<br>\n");
+            array_push($salida["D"], $check);
+            array_push($salida["E"], false);
+        }else{
+            array_push($salida["A"], $this->modoTest);
+            array_push($salida["B"], "inicioHTML".""."<br>\n");
+            array_push($salida["C"], "Error en " . __FILE__ . " Linea: " . __LINE__ . "<br>\n");
+            array_push($salida["D"], $check);
+            array_push($salida["E"], true);
+            throw new Exception($salida);
+        }
+        if(count($this->index)==0){
+            array_push($salida["A"], $this->modoTest);
+            array_push($salida["B"], "inicioHTML"."Error en " . __FILE__ . " Linea: " . __LINE__ ."<br>\n");
+            array_push($salida["C"], "El archivo " . __FILE__ . " linea " . __LINE__ . " La variable de contenidos no esta establecida" . "<br>\n");
+            array_push($salida["D"], $check);
+            array_push($salida["E"], true);
+            throw new Exception($salida);
+        }
+    } catch (Exception $ex){
+        echo $ex->getMessage();
+        exit;
+    }finally{
+        $ii=-1;
+        $ii=count($this->index);
+    }
+    
+    while($ii>0){
+        if($this->modoTest){
+            //echo "estamos en modo test <br> \n";
+            if (strpos($this->index[0], "<flowCode||value=>>'main'||position=>") !== false) {
+                array_shift($this->index);
+                $ii=count($this->index);
+                array_push($salida["A"], $this->modoTest);
+                array_push($salida["B"], "inicioHTML");
+                array_push($salida["C"], "index ".$ii." salida <br>\n");
+                array_push($salida["D"], $check);
+                array_push($salida["E"], false);
+                break;
+            }
+            $addInsert=array_shift($this->index);
+            array_push($this->salidaFinHTML, $addInsert);
+            $ii=count($this->index);
+            array_push($salida["A"], $this->modoTest);
+            array_push($salida["B"], "inicioHTML");
+            array_push($salida["C"], "index ".$ii);
+            array_push($salida["D"], $check);
+            array_push($salida["E"], false);
+            //echo "index ".$ii." recorriendo <br>\n";
+        }else{
+            //echo "NONONO estamos en modo test <br> \n";
+            if (strpos($this->index[0], "<flowCode||value=>>'main'||position=>") !== false) {
+                array_shift($this->index);
+                $ii=count($this->index);
+                break;
+            }
+            $addInsert=array_shift($this->index);
+            array_push($this->salidaFinHTML, $addInsert);
+            $ii=count($this->index);
+        }
+    }
