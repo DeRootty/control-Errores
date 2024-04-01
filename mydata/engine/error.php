616e5bc88d1029438f94e46d482bae1a43d47799
David <152688771+DeRootty@users.noreply.github.com>
2024-04-01 09:04:01 +0200

    Delete mydata directory

--- mydata/engine/error.php
+++
@@ -1,5 +0,0 @@
-<?php
-    header('HTTP/1.0 404 Not Found', true, 404);
-    
-    $datoUri = "El tip solicitado es" . $_GET['tip'] ?? "Se redirecciona a: ". $_SERVER['REQUEST_URI'];
-    echo "Esta pagina es de error: ".$datoUri."\n";
