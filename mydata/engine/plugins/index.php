ab43f6bb5600ebf8c936167b10cca74de5ab5de8
David <152688771+DeRootty@users.noreply.github.com>
2024-04-02 21:31:56 +0200

    Add files via upload

---
+++ mydata/engine/plugins/index.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+    try{
+        if(!file_exists("/srv/vhost/derootty.xyz/home/html/Dinamica/seguridad/ahead.php")){
+            throw new Exception("Fallo en el fundamento inherente a la seguridad. El archivo ".__FILE__." Viola el acceso al recurso");
+        }
+    } catch (Exception $ex) {
+        echo $ex->getMessage();
+        exit;
+    }finally{
+        include ("/srv/vhost/derootty.xyz/home/html/Dinamica/seguridad/ahead.php");
+    }
+    
