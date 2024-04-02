7489a75b11c985a35d7894ed41b21ae2b1e6c038
David <152688771+DeRootty@users.noreply.github.com>
2024-04-02 22:17:25 +0200

    Add files via upload

---
+++ Dinamica/fallos/EC_4XX/EC_431/index.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+
+    try{
+        if(!file_exists("/srv/vhost/derootty.xyz/home/html/Dinamica/seguridad/ahead.php")){
+            throw new Exception("Fallo en el fundamento inherente a la seguridad. El archivo ".__FILE__." Viola el acceso al recurso");
+        }
+    } catch (Exception $ex) {
+        echo $ex->getMessage();
+        exit;
+    }finally{
+        include ("/srv/vhost/derootty.xyz/home/html/Dinamica/seguridad/ahead.php");
+    }
+
+if(empty($_POST) || (!isset($_POST["dinamica"]) && !isset($_POST["leyenda"]))){
+    header("Location: /Dinamica/fallos/EC_4XX/EC_404/index.php?setdata=Pagina_no_Encontrada");
+}
+if(!isset($_GET)){
+    header('Location: /Dinamica/fallos/EC_4XX/EC_404/index.php?setData=directo');
+}
